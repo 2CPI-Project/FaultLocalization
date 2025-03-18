d886b50e3ee82a363d9da9e7121b50e20dcdc48d
DouaaGhofrane <125102130+DouaaGhofrane@users.noreply.github.com>
2025-03-18 17:40:07 +0100

    Update ScriptReader2.java

--- module2/ScriptReader2.java
+++ module2/ScriptReader2.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class ScriptReader2 {
 //call a bash script in ur java code and run it
   public static void main(String[] args) {
-    String[] cmd = new String[] {"/home/kali/core/module2/categorizeTests_cases.sh"}; //your script here
+    String[] cmd = new String[] {"./src/packages/categorizeTests_cases.sh"}; //your script here
     ProcessBuilder pb = new ProcessBuilder(cmd);
 
     try {
