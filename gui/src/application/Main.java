0816fa346e46f0ad830b5a6f18752a9751f4c2b5
DouaaGhofrane <125102130+DouaaGhofrane@users.noreply.github.com>
2025-03-20 06:02:54 +0100

    Add files via upload

---
+++ gui/src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+			Scene scene = new Scene(root);
+			String css = this.getClass().getResource("application.css").toExternalForm();
+			scene.getStylesheets().add(css);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(true);
+			primaryStage.setTitle("Debugger");
+			primaryStage.setMinWidth(610);
+			primaryStage.setMinHeight(430);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
