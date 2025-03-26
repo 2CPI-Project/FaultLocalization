324acab0736323ab13b0197f415696cd11ef4efd
DouaaGhofrane <125102130+DouaaGhofrane@users.noreply.github.com>
2025-03-26 17:17:00 +0100

    Update scriptreader.java

--- module1/scriptreader.java
+++ module1/scriptreader.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class scriptreader {
 //call a bash script in ur java code and run it
   public static void main(String[] args) {
-    String[] cmd = new String[] {"./src/packages/outputGenerator.sh"}; //your script here
+    String[] cmd = new String[] {"src/packages/outputGenerator.sh"}; //your script here
     ProcessBuilder pb = new ProcessBuilder(cmd);
 
     try {
